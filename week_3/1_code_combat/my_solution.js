4e3d7670905b92322a03dfae58bf226b1d9a50d0
armenvartan <vartan.armen@gmail.com>
2014-04-15 13:42:18 -0400

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,13 +1,95 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
- 
- 
- 
- 
+//Rescue Mission
+// move down x2
+// move right
+// move up x2
+// move right x2
+// move down
+// attack!
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+// Grab the Mushroom
+// move up
+// move right
+// move left
+// move up
+// kill
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
+
+//Drink Me
+// kill
+// move right
+// move down
+// move up
+// move right
+// leave no prisoners
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+//Taunt the Guards
+// delete the comments
+// move right x2
+// move up
+// move right
+// taunt
+// kill
+// move right x2
+this.moveRight();
+this.bustDownDoor();
+
+// Delete the "//" in front of each line below.
+this.moveRight();
+this.say("Hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+
+// Now get Phoebe to follow through the dungeon.
+this.moveRight();
+this.say("Follow me.");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight(); 
+this.say("My Butt!");
+this.say("Attack!");
+this.attackNearbyEnemy();
+this.attackNearbyEnemy();
+this.say("Follow me.");
+this.moveRight();
+
+// It's a Trap!
+// move down x2
+// yo!
+// move up x2
+// attack!
+this.moveDown();
+this.moveDown();
+this.say("Poopy");
+this.moveUp();
+this.moveUp();
+this.say("Attack!");
  
  
  
