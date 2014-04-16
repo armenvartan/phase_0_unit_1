bcd921b3fd214fb61d965877d3dcc7d76a6399cc
Armen Vartan <vartan.armen@gmail.com>
2014-04-16 12:59:45 -0400

    Added user stories

---
+++ week_3/5_group_project/gc1_user_stories.js
@@ -0,0 +1,3 @@
+As a user, I want to take a group of numbers and find out their sum.
+As a user, I want to take a group of numbers and find out their mean.
+As a user, I want to take a group of numbers and find out their median.
