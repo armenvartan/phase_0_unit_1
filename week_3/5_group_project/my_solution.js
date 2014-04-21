c463a4fd25293dbcf668aeaa05e210f60328df62
Armen Vartan <vartan.armen@gmail.com>
2014-04-20 21:44:26 -0400

    Last stuff for week 3

---
+++ week_3/5_group_project/my_solution.js
@@ -0,0 +1,94 @@
+function sum (array){
+		var rsum = 0;
+		for (i = 0; i < array.length; i++)
+		rsum = rsum + array[i];
+		total = rsum;
+		return total;
+};
+
+	function mean (array){
+		var rsum = 0;
+		for (i = 0; i < array.length; i++)
+		rsum = rsum + array[i];
+		total = rsum;
+		return total / array.length;
+};
+
+	function median (array){
+		if (array.length % 2 === 0) {
+			return (array[(array.length / 2) - 1] + array[array.length/2]) / 2;;
+	}
+	else {
+			return array[Math.floor(array.length/2)];
+	}
+};
+
+
+oddLengthArray  = [1, 2, 3, 4, 5, 5, 7];
+evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7];
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+// tests for sum
+assert(
+  (sum instanceof Function),
+  "sum should be a Function.",
+  "1. "
+)
+
+assert(
+  sum(oddLengthArray) === 27,
+  "sum should return the sum of all elements in an array with an odd length.",
+  "2. "
+)
+
+assert(
+  sum(evenLengthArray) === 43,
+  "sum should return the sum of all elements in an array with an even length.",
+  "3. "
+)
+
+// tests for mean
+assert(
+  (mean instanceof Function),
+  "mean should be a Function.",
+  "4. "
+)
+
+assert(
+  mean(oddLengthArray) === 3.857142857142857,
+  "mean should return the average of all elements in an array with an odd length.",
+  "5. "
+)
+
+assert(
+  mean(evenLengthArray) === 5.375,
+  "mean should return the average of all elements in an array with an even length.",
+  "6. "
+)
+
+// tests for median
+assert(
+  (median instanceof Function),
+  "median should be a Function.",
+  "7. "
+)
+
+assert(
+  median(oddLengthArray) === 4,
+  "median should return the median value of all elements in an array with an odd length.",
+  "8. "
+)
+
+assert(
+  median(evenLengthArray) === 5.5,
+  "median should return the median value of all elements in an array with an even length.",
+  "9. "
+)
--- week_3/8_gps1_2/our_solution.js
+++ week_3/8_gps1_2/our_solution.js
@@ -1,21 +1,39 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1.Armen
+//  2.Emmanuel
 
 
 // 1. "YOU SIGNED... WHO?!"
+var toshiroMifune = {
+    name: "Toshiro Mifune",
+    age: 90,
+    quote: "YAMERO"
+};
+toshiroMifune.name
 
+var toshiroMifune = new Object()
+
+toshiroMifune.name = "Toshiro Mifune"
+toshiroMifune.age = "90"
+
+var toshiroMifune = {}
+//create an object in JS storing age, name, quote
 
 
 
 // 2. "Here they Come!"
-
-
-
-
-
+// constructor function
+var Client = function(name, age, quote) {
+  this.name = name,
+  this.age = age,
+  this.quote = quote
+};
+var adamSandler = new Client("Adam Sandler", 47, "That's your home! Are you too good for your home?!");
+var kristinBell = new Client("Kristin Bell", 33, "Do you wanna build a snowman?");
+var jimCarrey = new Client("Jim Carrey", 52, "...So you're telling me there's a chance? YEAH!");
+aList = [adamSandler, kristinBell, jimCarrey, toshiroMifune];
 // 3. "TIME IS MONEY!"
 
 //YOUR CODE HERE!
@@ -30,11 +48,19 @@ shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to wh
 
 
 // 4. "SHOW 'EM OFF!"
-
-
-
+displayClient  = function(client){
+    alert("Please meet " + client.name),
+    alert("At the young age of " + client.age),
+    alert(quote)
+};
+for (i=0, i < aList.length, i++){
+    displayClient(aList[i])
+};
 
 // ** BONUS **
 
 
 //  Your Reflection:
+// I was pretty off for this assignment. Emmanuel was really helpful. He let me try to work through things,
+// and didn't give me any clues until I was definitely stuck. I learned how 'this' works.
+// This assignment also helped solidify my JS flow.
